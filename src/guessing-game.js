7eb4e3169f0e5b25a1c9a68a6d83113a961aafe8
Diana Butiakova <butyakova01@mail.ru>
2024-10-28 01:38:52 +0600

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
+    #min;
+    #max;
+    #lastQuess;
+
     constructor() {}
 
     setRange(min, max) {
-
+        this.#min = min;
+        this.#max = max; 
     }
 
     guess() {
-
+        this.#lastQuess = Math.ceil((this.#max + this.#min) / 2);
+        return this.#lastQuess;
     }
 
     lower() {
-
+        if (this.#max !== this.#min) this.#max = this.#lastQuess;
     }
 
     greater() {
-
+        if (this.#max !== this.#min) this.#min = this.#lastQuess;
     }
 }
 
